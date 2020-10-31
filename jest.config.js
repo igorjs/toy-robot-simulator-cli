e7b564edd49f8e817d1a7e903aaaf26decf6cc77
Igor Santos <igorjosesantos@outlook.com>
2020-11-01 02:21:36 +1100

    chore: initial project setup

---
+++ jest.config.js
@@ -0,0 +1,25 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/en/configuration.html
+ */
+module.exports = {
+  preset: 'ts-jest',
+  coverageDirectory: '.coverage',
+  coverageProvider: 'v8',
+  coverageThreshold: {
+    global: {
+      branches: 50,
+      functions: 50,
+      lines: 50,
+      statements: 50,
+    },
+  },
+  collectCoverageFrom: ['src/**/*.{ts,js}', '!src/**/*.(test|spec).{ts,js}', '!src/cli/**', '!**/node_modules/**'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  testMatch: ['**/*.(test|spec).(ts|js)'],
+  testEnvironment: 'node',
+  setupFiles: ['./jest.setup.js'],
+}
