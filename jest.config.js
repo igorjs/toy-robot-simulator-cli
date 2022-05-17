31b86f3fdceb8c5ca3b83a1300412ffade58f32c
igor.js <igor@igorjs.io>
2022-05-17 01:14:40 -0300

    chore: remove coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -9,15 +9,6 @@ module.exports = {
   restoreMocks: true,
   coverageDirectory: '.coverage',
   coverageProvider: 'v8',
-  coverageThreshold: {
-    global: {
-      branches: 70,
-      functions: 70,
-      lines: 70,
-      statements: 70,
-    },
-  },
-  // collectCoverageFrom: ['src/**/*.{ts,js}', '!src/**/*.(test|spec).{ts,js}', '!src/cli/**', '!**/node_modules/**'],
   collectCoverageFrom: [
     'src/**/*.{ts,js}',
     '!src/index.{ts,js}',
